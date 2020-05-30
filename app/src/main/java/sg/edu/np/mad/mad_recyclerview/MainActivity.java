857d15a2209b67b967c5695f885f86bd0f3db8e6
BuildTools <unconfigured@null.spigotmc.org>
2020-05-31 01:19:39 +0800

    practical done

--- app/src/main/java/sg/edu/np/mad/mad_recyclerview/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/mad_recyclerview/MainActivity.java
@@ -1,20 +1,51 @@
 package sg.edu.np.mad.mad_recyclerview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    String TAG = "recyclerview1";
+    ArrayList<String> toDoList = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        final ArrayList<String> list = new ArrayList<>();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.v(TAG, "starting");
+        Button addButton = findViewById(R.id.addButton);
+        final EditText etAdd = findViewById(R.id.editTextAdd);
+        final RecyclerView recyclerView = findViewById(R.id.toDoList);
+        recyclerFunction(list);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String newItem = etAdd.getText().toString();
+                if (newItem.isEmpty()) {
+                    Log.v(TAG, "Input is empty");
+                }
+                else {
+                    list.add(newItem);
+                    showNewEntry(recyclerView, list);
+                    etAdd.setText("");
+                }
+            }
+        });
+
+
+
     }
 
     /**
@@ -31,5 +62,14 @@ public class MainActivity extends AppCompatActivity {
         //auto hide keyboard after entry
         InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
         imm.hideSoftInputFromWindow(rv.getWindowToken(), 0);
+    }
+
+    private void recyclerFunction(List<String> sList) {
+        RecyclerView recyclerView = findViewById(R.id.toDoList);
+        toDoAdapter mAdaptor = new toDoAdapter(sList);
+        LinearLayoutManager mLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(mAdaptor);
     }
 }
